10a851a5e4019ad29d782edeaa90eb192d73cd9b
Rishab <rishabkanwal@gmail.com>
2016-04-16 20:40:53 -0600

    Added Tweet model

--- django_api/api/models.py
+++ django_api/api/models.py
@@ -7,9 +7,9 @@ import uuid
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
-# ExampleModel for now
-class ExampleModel(Model):
-    example_id    = columns.UUID(primary_key=True, default=uuid.uuid4)
-    example_type  = columns.Integer(index=True)
-    created_at    = columns.DateTime()
-    description   = columns.Text(required=False)
+class Tweet(Model):
+    id = columns.UUID(primary_key = True, default=uuid.uuid4)
+    candidate = columns.Text(required = True)
+    created = columns.DateTime(required = True)
+    sentiment = columns.Integer()
+    text = columns.Text(required = True)
