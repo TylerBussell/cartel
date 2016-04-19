58043bdbe8de5ba419ef465911d90a4d7453a38a
Tyler Bussell <Bussell.Tyler@gmail.com>
2016-04-19 15:44:13 -0600

    Updated DB models.

--- django_api/api/models.py
+++ django_api/api/models.py
@@ -8,10 +8,33 @@ import datetime
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
-class Tweet(Model):
+class Candidate(Model):
+
+    __abstract__ = True
+    __keyspace__ = 'prod_db'
+
     candidate = columns.Text(required = True, primary_key = True)
     created_at = columns.Integer(required = True, primary_key = True)
-    sentiment = columns.Float()
+    sentiment = columns.Float(required = False)
     text = columns.Text(required = True)
     user = columns.Text(required = False)
     tid = columns.Text(required = False)
+
+
+class Bernie(Candidate):
+    __table_name__ = 'bernie'
+
+class Cruz(Candidate):
+    __table_name__ = 'cruz'
+
+class Hillary(Candidate):
+    __table_name__ = 'hillary'
+
+class Trump(Candidate):
+    __table_name__ = 'trump'
+
+class Dem(Candidate):
+    __table_name__ = 'democrat'
+
+class Rep(Candidate):
+    __table_name__ = 'republican'
