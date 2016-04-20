d5ce126c2216793587b06a4cc16a6f821dec38a9
Lgueg28 <loic.guegan@colorado.edu>
2016-04-19 19:00:42 -0600

    Lots of Aesthetic Changes

--- web_frontend/app/analysisView/analysisView.js
+++ web_frontend/app/analysisView/analysisView.js
@@ -9,53 +9,56 @@ angular.module('myApp.analysisView', ['ngRoute', 'highcharts-ng', "ngTable"])
   });
 }])
 
-.controller('AnalysisViewCtrl', ['$scope', 'NgTableParams', 'cartelAPI', function($scope, NgTableParams, cartelAPI) {
+.controller('AnalysisViewCtrl', ['$scope', 'NgTableParams', 'cartelAPIAll', function($scope, NgTableParams, cartelAPIAll) {
 
+	$scope.dataLoaded = true;
+	
     $scope.options = {
         type: 'column'
     }
-
+    
     $scope.highchartsNG = {
-        options: {
-            chart: {
-                type: 'column'
-            }
-        },
-        series: [{
-        	name: "Trump Sentiment Analysis",
-            data: [10, 15, 12, 8, 7, 1, 15, 10 , 8, 13]
-        },
-        {
-        	name: "Sanders Sentiment Analysis",
-            data: [8, 10, 6, 15, -1, 0, 3, 18 , 20, 10]
-        }],
-        yAxis: {
-            title: {
-                text: 'Sentiment'
-            }
-        },
-        title: {
-            text: 'Twitter Results for Trump'
-        },
-        xAxis: {
-            categories: [
-                'Jan',
-                'hot',
-                'Mar',
-                'Apr',
-                'May',
-                'Jun',
-                'Jul',
-                'Aug',
-                'Sep',
-                'Oct',
-                'Nov',
-                'Dec'
-            ],
-            crosshair: true
-        },
-        loading: false
-    }
+	        options: {
+	            chart: {
+	                type: 'column',
+	                marginTop: 50
+	            }
+	        },
+	        series: [{
+	        	name: "Sample1 Sentiment Analysis",
+	            data: [10, 15, 12, 8, 7, 1, 15, 10 , 8, 13]
+	        },
+	        {
+	        	name: "Sample2 Sentiment Analysis",
+	            data: [8, 10, 6, 15, -1, 0, 3, 18 , 20, 10]
+	        }],
+	        yAxis: {
+	            title: {
+	                text: 'Sentiment'
+	            }
+	        },
+	        title: {
+	            text: 'Twitter Results for Sample1, Sample2'
+	        },
+	        xAxis: {
+	            categories: [
+	                'Jan',
+	                'Feb',
+	                'Mar',
+	                'Apr',
+	                'May',
+	                'Jun',
+	                'Jul',
+	                'Aug',
+	                'Sep',
+	                'Oct',
+	                'Nov',
+	                'Dec'
+	            ],
+	            crosshair: true
+	        },
+	        loading: false
+	    }
     
     var data = [
             {
@@ -76,7 +79,41 @@ angular.module('myApp.analysisView', ['ngRoute', 'highcharts-ng', "ngTable"])
     });
     
     $scope.runQuery = function() {
+    	
+    	$('#statisticDiv').hide();
+    	$scope.dataLoaded = false;
+    	
     	console.log("Run Query Button Clicked");
+    	var input = $('#queryInput').val();
+    	
+    	cartelAPIAll.queryAll()
+    		.$promise.then(
+    			function( value ) {
+    		
+    			},
+    			function( error ) {
+    				console.log( "Bad Request" );
+    			}
+    		);
+    	//code before the pause
+		setTimeout(function(){
+			$scope.highchartsNG.title = {
+	            text: 'Twitter Results for ' + input
+ 	        },
+			$scope.highchartsNG.series = [{
+	    	        	name: "Trump Sentiment Analysis",
+	    	            data: [10, 15, 12, 8, 7, 1, 15, 10 , 8, 13]
+	    	        },
+	    	        {
+	    	        	name: "Sanders Sentiment Analysis",
+	    	            data: [8, 10, 6, 15, -1, 0, 3, 18 , 20, 10]
+	    	        }];
+			
+			 $scope.dataLoaded = true;
+			 $scope.$apply();
+			$('#statisticDiv').show();
+		}, 2000);
+    
     };
     
     $(function() {
