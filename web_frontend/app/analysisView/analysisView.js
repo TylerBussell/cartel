f67a7f332ee8d6e70684be608facc1db908e9905
Lgueg28 <loic.guegan@colorado.edu>
2016-04-20 17:27:14 -0600

    UI changes, added tabs for candidates and hooked up api calls

--- web_frontend/app/analysisView/analysisView.js
+++ web_frontend/app/analysisView/analysisView.js
@@ -3,120 +3,152 @@
 angular.module('myApp.analysisView', ['ngRoute', 'highcharts-ng', "ngTable"])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/analysisView', {
+  $routeProvider.when('/analysisView/:viewParam', {
     templateUrl: 'analysisView/analysisView.html',
     controller: 'AnalysisViewCtrl'
   });
 }])
 
-.controller('AnalysisViewCtrl', ['$scope', 'NgTableParams', 'cartelAPIAll', function($scope, NgTableParams, cartelAPIAll) {
+.controller('AnalysisViewCtrl', ['$scope', 'NgTableParams', 'cartelAPITrump', 'cartelAPIHillary', 'cartelAPIBernie', 'cartelAPICruz', 'cartelAPIDemocrat', 'cartelAPIRepublican', '$http', '$routeParams',
+                                 function($scope, NgTableParams, cartelAPITrump, cartelAPIHillary, cartelAPIBernie, cartelAPICruz, cartelAPIDemocrat, cartelAPIRepublican, $http, $routeParams) {
 
-	$scope.dataLoaded = true;
+	$scope.dataLoaded = false;
+	
+	var view = $routeParams.viewParam;
 	
     $scope.options = {
         type: 'column'
     }
     
-    $scope.highchartsNG = {
-	        options: {
-	            chart: {
-	                type: 'column',
-	                marginTop: 50
-	            }
-	        },
-	        series: [{
-	        	name: "Sample1 Sentiment Analysis",
-	            data: [10, 15, 12, 8, 7, 1, 15, 10 , 8, 13]
-	        },
-	        {
-	        	name: "Sample2 Sentiment Analysis",
-	            data: [8, 10, 6, 15, -1, 0, 3, 18 , 20, 10]
-	        }],
-	        yAxis: {
-	            title: {
-	                text: 'Sentiment'
-	            }
-	        },
-	        title: {
-	            text: 'Twitter Results for Sample1, Sample2'
-	        },
-	        xAxis: {
-	            categories: [
-	                'Jan',
-	                'Feb',
-	                'Mar',
-	                'Apr',
-	                'May',
-	                'Jun',
-	                'Jul',
-	                'Aug',
-	                'Sep',
-	                'Oct',
-	                'Nov',
-	                'Dec'
-	            ],
-	            crosshair: true
-	        },
-	        loading: false
-	    }
+    $scope.chartTitle = null;
+    $scope.tableData = null;
     
-    var data = [
-            {
-    			link: "FMA_TEST",
-    			text: "Blase Blase Blase"
-    		},
-    		{
-    			link: "FMA_TEST",
-    			text: "Blase Blase Blase"
-    		}
-    ];
+    switch (view) {
+    	case "trump":
+    		$scope.chartTitle = "Donald Trump";
+    		cartelAPITrump.queryAll()
+	    		.$promise.then(
+	    			function( value ) {
+	    				$scope.tableData = value;
+	    				$scope.buildChart();
+	    			},
+	    			function( error ) {
+	    				console.log( "Bad Request" );
+	    			}
+	    	);
+    		break;
+    	case "clinton":
+    		$scope.chartTitle = "Hillary Clinton";
+    		cartelAPIHillary.queryAll()
+	    		.$promise.then(
+	    			function( value ) {
+	    				$scope.tableData = value;
+	    				$scope.buildChart();
+	    			},
+	    			function( error ) {
+	    				console.log( "Bad Request" );
+	    			}
+	    	);
+    		break;
+    	case "sanders":
+    		$scope.chartTitle = "Bernie Sanders";
+    		cartelAPIBernie.queryAll()
+	    		.$promise.then(
+	    			function( value ) {
+	    				$scope.tableData = value;
+	    				$scope.buildChart();
+	    			},
+	    			function( error ) {
+	    				console.log( "Bad Request" );
+	    			}
+	    	);
+    		break;
+    	case "cruz":
+    		$scope.chartTitle = "Ted Cruz";
+    		cartelAPITCruz.queryAll()
+	    		.$promise.then(
+	    			function( value ) {
+	    				$scope.tableData = value;
+	    				$scope.buildChart();
+	    			},
+	    			function( error ) {
+	    				console.log( "Bad Request" );
+	    			}
+	    	);
+    		break;
+    	case "democrat":
+    		$scope.chartTitle = "Democrat";
+    		cartelAPIDemocrat.queryAll()
+	    		.$promise.then(
+	    			function( value ) {
+	    				$scope.tableData = value;
+	    				$scope.buildChart();
+	    			},
+	    			function( error ) {
+	    				console.log( "Bad Request" );
+	    			}
+	    	);
+    		break;
+    	case "republican":
+    		$scope.chartTitle = "Republican";
+    		cartelAPIRepublican.queryAll()
+	    		.$promise.then(
+	    			function( value ) {
+	    				$scope.tableData = value;
+	    				$scope.buildChart();
+	    			},
+	    			function( error ) {
+	    				console.log( "Bad Request" );
+	    			}
+	    	);
+    		break;
+    }
     
-    $scope.tableParams = new NgTableParams({
-        page: 1,            // show first page
-        count: 10			// count per page
-    }, {
-        data: data
-    });
-    
-    $scope.runQuery = function() {
+    $scope.buildChart = function() {
+    	$scope.highchartsNG = {
+    	        options: {
+    	            chart: {
+    	                type: 'column',
+    	                marginTop: 50
+    	            }
+    	        },
+    	        series: [{
+    	        	name: "Tweets",
+    	            data: [10, 15, 12, 8, 7, 1, 15, 10 , 8, 13]
+    	        }],
+    	        yAxis: {
+    	            title: {
+    	                text: 'Sentiment'
+    	            }
+    	        },
+    	        title: {
+    	            text: 'Sentiment Analysis for ' + $scope.chartTitle
+    	        },
+    	        xAxis: {
+    	            categories: [
+    	                'Jan',
+    	                'Feb',
+    	                'Mar',
+    	                'Apr',
+    	                'May',
+    	                'Jun',
+    	                'Jul',
+    	                'Aug',
+    	                'Sep',
+    	                'Oct',
+    	                'Nov',
+    	                'Dec'
+    	            ],
+    	            crosshair: true
+    	        },
+    	        loading: false
+    	    }
     	
-    	$('#statisticDiv').hide();
-    	$scope.dataLoaded = false;
-    	
-    	console.log("Run Query Button Clicked");
-    	var input = $('#queryInput').val();
-    	
-    	cartelAPIAll.queryAll()
-    		.$promise.then(
-    			function( value ) {
-    		
-    			},
-    			function( error ) {
-    				console.log( "Bad Request" );
-    			}
-    		);
-    	//code before the pause
-		setTimeout(function(){
-			$scope.highchartsNG.title = {
-	            text: 'Twitter Results for ' + input
- 	        },
-			$scope.highchartsNG.series = [{
-	    	        	name: "Trump Sentiment Analysis",
-	    	            data: [10, 15, 12, 8, 7, 1, 15, 10 , 8, 13]
-	    	        },
-	    	        {
-	    	        	name: "Sanders Sentiment Analysis",
-	    	            data: [8, 10, 6, 15, -1, 0, 3, 18 , 20, 10]
-	    	        }];
-			
-			 $scope.dataLoaded = true;
-			 $scope.$apply();
-			$('#statisticDiv').show();
-		}, 2000);
+    	$scope.dataLoaded = true;
+
+    }
     
-    };
-    
-    $scope.toogleLine = function() {
+    $scope.toggleLine = function() {
     	$scope.highchartsNG.options = {
     	    chart: {
     	        type: 'line',
@@ -126,7 +158,7 @@ angular.module('myApp.analysisView', ['ngRoute', 'highcharts-ng', "ngTable"])
     	$scope.$apply();
     }
     
-    $scope.toogleBar = function() {
+    $scope.toggleBar = function() {
     	$scope.highchartsNG.options = {
     	    chart: {
     	        type: 'column',
