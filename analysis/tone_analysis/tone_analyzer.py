7c58ebbdbc18404d14a9b9f116d305d38b1053c3
Tyler Bussell <Bussell.Tyler@gmail.com>
2016-04-22 17:08:50 -0600

    Updated tone analyzer

--- analysis/tone_analysis/tone_analyzer.py
+++ analysis/tone_analysis/tone_analyzer.py
@@ -11,25 +11,30 @@ def analyze_tone(candidate, in_fn, out_fn):
                                        version='2016-02-11')
 
     with open(in_fn, 'r') as in_f, open(out_fn, 'a') as out_f:
-        for line in in_f:
-            out_data = {'candidate':candidate, 'sentiment':-1.0}
-            in_data = json.loads(line)
-            tone_data = tone_analyzer.tone(text=in_data['text'])
-            emotion = tone_data['document_tone']['tone_categories'][0]['tones']
-            # writing = tone_data['document_tone']['tone_categories'][1]['tones']
-            social = tone_data['document_tone']['tone_categories'][2]['tones']
+        for i, line in enumerate(in_f):
+            try:
+                out_data = {'candidate':candidate, 'sentiment':-1.0}
+                in_data = json.loads(line)
+                tone_data = tone_analyzer.tone(text=in_data['text'])
+                emotion = tone_data['document_tone']['tone_categories'][0]['tones']
+                # writing = tone_data['document_tone']['tone_categories'][1]['tones']
+                social = tone_data['document_tone']['tone_categories'][2]['tones']
 
-            for e in emotion:
-                out_data[e['tone_name'].lower()] = e['score']
-            for s in social:
-                out_data[s['tone_name'].lower()] = s['score']
+                for e in emotion:
+                    out_data[e['tone_name'].lower()] = e['score']
+                for s in social:
+                    out_data[s['tone_name'].lower()] = s['score']
 
-            out_data.update(in_data)
-            out_data['user'] = out_data.pop('uid')
-            out_data['range'] = out_data.pop('emotional range')
-            json.dump(out_data, out_f)
+                out_data.update(in_data)
+                out_data['user'] = out_data.pop('uid')
+                out_data['range'] = out_data.pop('emotional range')
+                json.dump(out_data, out_f)
+                out_f.write('\n')
+            except Exception as e:
+                print(e.args, i)
 
-analyze_tone('trump', '/home/tyler/centipede.json.1', 'tone_test_file')
+
+analyze_tone('trump', '/home/ubuntu/centipede.json.1', '/home/ubuntu/tone_test_file')
 
 
 
