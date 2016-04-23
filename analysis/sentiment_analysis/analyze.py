5dbbddbd5aee636b3d2e16834c36734ac44e6d9b
Tyler Bussell <Bussell.Tyler@gmail.com>
2016-04-23 14:52:09 -0600

    Added sentiment analysis drive script

---
+++ analysis/sentiment_analysis/analyze.py
@@ -0,0 +1,38 @@
+__author__ = 'tyler'
+
+import interface
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input',
+                    nargs=1,
+                    help='Full path to jsonl file')
+parser.add_argument('output',
+                    nargs=1,
+                    help='Full path to output file')
+args = parser.parse_args()
+
+def analyze(args):
+    with open(args.input, 'r') as in_f, open(args.output, 'a') as out_f:
+        for line in in_f:
+            in_data = json.loads(line)
+            sent = interface.predictTweet(in_data['text'])
+            in_data['sentiment'] = sent['pos']
+            json.dump(in_data, out_f)
+            out_f.write('\n')
+
+if __name__ == 'main':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input',
+                        nargs=1,
+                        help='Full path to jsonl file')
+    parser.add_argument('output',
+                        nargs=1,
+                        help='Full path to output file')
+    args = parser.parse_args()
+    analyze(args)
+
+
+
