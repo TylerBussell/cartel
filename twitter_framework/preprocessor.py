7f3c97977e2b081d4d23ff764bda16bb3029a938
Tyler Bussell <Bussell.Tyler@gmail.com>
2016-04-26 20:38:58 -0600

    Updated preprocessor to work better with spark. Added spark script for batch files to db.

--- twitter_framework/preprocessor.py
+++ twitter_framework/preprocessor.py
@@ -1,33 +1,20 @@
 __author__ = 'tyler'
 
 
-import re
 
-from itertools import groupby
-from nltk.corpus import stopwords
-from nltk.stem.snowball import EnglishStemmer
-from nltk.tokenize.casual import TweetTokenizer
-
-
-_url_ptn = re.compile(r'(www\d{0,3}\.[^\s]+)|(https?://[^\s]+)')
-_at_ptn = re.compile(r'\.?@([^\s]+)')
-_hash_ptn = re.compile(r'#([^\s]+)')
-_stop_words = set(stopwords.words('english'))
-_tokenizer = TweetTokenizer(preserve_case=False)
-
-
-
-def remove_all(tweet):
-    return _url_ptn.sub('', _hash_ptn.sub(r'\1', _at_ptn.sub(r'\1', tweet)))
-
-def remove_at(text):
-    return _at_ptn.sub(r'\1', text)
-
-def remove_hash(text):
-    return _hash_ptn.sub(r'\1', text)
-
-def remove_url(text):
-    return _url_ptn.sub('', text)
-
-def remove_repeats(text):
-    return ''.join(''.join(s)[:2] for _, s in groupby(text))
+def spark_tokenize(tweet):
+    import re
+    import string
+    from itertools import groupby
+    from nltk.tokenize import word_tokenize
+    from nltk.corpus import stopwords
+    hash_ptn = re.compile(r'#([^\s]+)')
+    other_ptn = re.compile(r'(www\d{0,3}\.[^\s]+)|(https?://[^\s]+))')
+    punct = set(string.punctuation)
+    stop = set(stopwords.words('english'))
+    tokens = []
+    text = hash_ptn.sub(r'\1', other_ptn.sub('', tweet))
+    for token in word_tokenize(text):
+        if token not in stop and token not in punct:
+            tokens.append(''.join(''.join(s)[:2] for _, s in groupby(token)))
+    return tokens
